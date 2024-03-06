f116c30ec4d1fa22ed9373ab555fc29fc26894bf
Monish Atom <monishatom@outlook.com>
2024-03-06 16:38:37 +0530

    feat(banner.tsx): Update banner image and content
    - Update the banner image to use a new image file `female-wide-11.jpg` with higher quality and different dimensions.
    - Adjust the height of the banner section to be 75% of the viewport height.
    - Modify the layout and content of the banner section:
      - Change the text in the heading to "Vintage Vogue".
      - Add a subheading with the text "Retro Women's Glam".
      - Add a button with the text "Shop now" for users to click and navigate to the shop page.
    
    feat(images): Add new banner images
    
    - Add two new image files `female-wide-11.jpg` and `female-wide-12.jpg` to the `public/images` directory.
    - These images will be used as banner images on the website.
    
    feat(images): Add new image file `male-wide-02.jpg`
    
    - Add a new image file `male-wide-02.jpg` to the `public/images` directory.
    - This image will be used for a different section of the website.

--- components/banner.tsx
+++ components/banner.tsx
@@ -4,21 +4,35 @@ type Props = {};
 
 const Banner = (props: Props) => {
   return (
-    <section className='relative flex h-[80svh] items-center justify-center bg-amber-200 transition-all md:h-[100svh]'>
-      <div className='relative h-full w-full overflow-clip'>
+    <section className='relative flex h-[75svh] items-center justify-center bg-amber-200 transition-all md:h-[100svh]'>
+      <div className='relative flex aspect-auto h-full w-full overflow-clip'>
         <Image
-          width={1024}
-          height={768}
-          quality={75}
-          src={'/images/clothing-wide-02.jpg'}
+          width={1184}
+          height={664}
+          quality={100}
+          src={'/images/female-wide-11.jpg'}
           alt='banner-image'
-          className='h-full w-full animate-scale-up object-cover object-top [animation-range:0px_80%] [animation-timeline:scroll(y)] '
+          className='h-full w-full animate-scale-up object-cover [animation-range:0px_80%] [animation-timeline:scroll(y)]'
           priority
         />
-        <div className='absolute top-0 flex h-full w-full items-end bg-black/30 p-8 md:p-24'>
-          <h1 className='animate-translate-up text-4xl font-semibold text-fground [animation-range:0px_80%] [animation-timeline:scroll(y)] md:text-6xl lg:text-8xl'>
-            Shop the trends
-          </h1>
+        <div
+          className='absolute top-0 flex h-full w-full flex-col justify-end
+         gap-4 bg-black/30 p-8 text-right text-fground md:p-24'
+        >
+          <div className='flex flex-col items-start justify-start'>
+            <h1 className='animate-translate-up text-5xl font-semibold  [animation-range:0px_80%] [animation-timeline:scroll(y)] md:text-6xl'>
+              Vintage Vogue
+            </h1>
+            <h2 className='mt-2 animate-translate-up text-lg font-medium text-fground/85 [animation-range:0px_80%] [animation-timeline:scroll(y)] md:text-2xl'>
+              Retro Women&apos;s Glam
+            </h2>
+
+            <div className='mt-6'>
+              <button className='rounded bg-fground px-6 py-3 text-bground'>
+                Shop now
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </section>
