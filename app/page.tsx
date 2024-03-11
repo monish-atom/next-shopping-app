012d34b2f3422fd8e426cd2b499f3c289674c49c
Monish Atom <monishatom@outlook.com>
2024-03-11 15:53:25 +0530

    feat(page.tsx): add Accessories and Reviews sections to the Home page to enhance user experience and showcase additional content feat(page.tsx): add Link component to showcase items in the ShowCase section for better navigation feat(page.tsx): add Accessories section with an image and description to promote additional products feat(page.tsx): add Reviews section with customer testimonials to build trust and credibility feat(page.tsx): add new image 'accessories-05.jpg' for the Accessories section

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import { hmdata } from '@/lib/data';
 import { Testimonials } from '@/lib/testimonials';
 import { Star } from 'lucide-react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
-  const { testimonials } = Testimonials;
   return (
     <main className='relative z-40 bg-white'>
       <Banner />
@@ -18,45 +18,8 @@ export default function Home() {
         <Products />
       </section>
       <ShowCase />
-      <section className='h-full w-svw py-16'>
-        <div className='aspect-auto h-[80vh]'>
-          <Image
-            width={1184}
-            height={664}
-            src={'/images/accessories-01.jpg'}
-            alt={'Accessories'}
-            className='h-full w-full object-cover shadow-sm'
-          />
-        </div>
-      </section>
-
-      <section className='max-width h-full px-4 pb-36 pt-16 md:px-8'>
-        <h2 className='font-bricolage-grotesque text-xl font-medium lg:text-2xl'>
-          Customer reviews
-        </h2>
-        <div className='mt-8 flex gap-2 overflow-x-scroll'>
-          {testimonials.map((testimonial) => (
-            <div
-              key={testimonial.id}
-              className='h-72 w-96 flex-shrink-0 bg-amber-100/60 p-8 shadow-sm'
-            >
-              <h2 className='font-bricolage-grotesque font-medium md:text-lg'>
-                {testimonial.title}
-              </h2>
-              <div className='mt-4 flex items-center gap-1'>
-                {[...Array(5)].fill(0).map((_, index) => (
-                  <Star
-                    key={index}
-                    size={18}
-                    className='fill-yellow-300 text-yellow-300'
-                  />
-                ))}
-              </div>
-              <p className='mt-4 text-pretty'>{testimonial.feedback}</p>
-            </div>
-          ))}
-        </div>
-      </section>
+      <Accessories />
+      <Reviews />
     </main>
   );
 }
@@ -134,7 +97,11 @@ const ShowCase = () => {
 
       <div className='relative mt-8 grid h-full grid-cols-1 gap-2 md:grid-cols-3 lg:gap-4'>
         {showCaseData.map((data) => (
-          <div key={data.id} className='relative h-full w-full overflow-clip'>
+          <Link
+            href={`/${data.category.toLowerCase()}`}
+            key={data.id}
+            className='relative h-full w-full overflow-clip'
+          >
             <Image
               width={400}
               height={400}
@@ -147,6 +114,74 @@ const ShowCase = () => {
                 {data.title}
               </h2>
             </div>
+          </Link>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+const Accessories = () => {
+  return (
+    <section className='relative my-16 h-full w-svw'>
+      <div className='aspect-auto h-[75vh]'>
+        <Image
+          width={1184}
+          height={664}
+          quality={100}
+          src={'/images/accessories-05.jpg'}
+          alt={'Accessories'}
+          className='h-full w-full object-cover object-center'
+        />
+      </div>
+      <div className='absolute top-0 flex h-full w-full items-center justify-start bg-black/30 p-56 text-white'>
+        <div className='flex max-w-lg flex-col gap-4'>
+          <h2 className='font-bricolage-grotesque font-semibold lg:text-5xl'>
+            Complete Your Look with Trendsetting Extras
+          </h2>
+          <p className='text-secondary/90'>
+            From Luxurious Leather to Fashion-Forward Footwear, Uncover Your
+            Signature Style
+          </p>
+          <Button
+            variant={'secondary'}
+            size={'lg'}
+            className='w-fit rounded-none'
+          >
+            <span>Shop Now</span>
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const Reviews = () => {
+  const { testimonials } = Testimonials;
+  return (
+    <section className='max-width h-full px-4 pb-36 pt-16 md:px-8'>
+      <h2 className='font-bricolage-grotesque text-xl font-medium lg:text-2xl'>
+        Customer reviews
+      </h2>
+      <div className='mt-8 flex gap-2 overflow-x-scroll'>
+        {testimonials.map((testimonial) => (
+          <div
+            key={testimonial.id}
+            className='h-72 w-96 flex-shrink-0 bg-amber-100/60 p-8 shadow-sm'
+          >
+            <h2 className='font-bricolage-grotesque font-medium md:text-lg'>
+              {testimonial.title}
+            </h2>
+            <div className='mt-4 flex items-center gap-1'>
+              {[...Array(5)].fill(0).map((_, index) => (
+                <Star
+                  key={index}
+                  size={18}
+                  className='fill-yellow-300 text-yellow-300'
+                />
+              ))}
+            </div>
+            <p className='mt-4 text-pretty'>{testimonial.feedback}</p>
           </div>
         ))}
       </div>
